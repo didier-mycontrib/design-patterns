00712576311cdce403272fecc1cf12fe0b70846d
didier defrance <didier@d-defrance.fr>
2016-05-25 10:23:38 +0200

    with git url scripts

---
+++ sync_push_toGitHub.bat
@@ -0,0 +1,4 @@
+git remote add GitHubMyContribOrigin https://github.com/didier-mycontrib/design-patterns.git
+git push -u GitHubMyContribOrigin master
+REM didier-mycontrib / gh14.....sm..x / didier@d-defrance.fr
+pause
