3d7df62717613885f12929bf14b9a56ac2d77249
didier defrance <didier@d-defrance.fr>
2019-11-29 07:37:18 +0100

    acl / chain of responsability

--- design_patterns-ext/src/main/java/tp/basic/link/AbstractLink.java
+++ design_patterns-ext/src/main/java/tp/basic/link/AbstractLink.java
@@ -1,6 +1,16 @@
 package tp.basic.link;
 
 //maillon abstrait (chain of responsability)
-public class AbstractLink {
-	AbstractLink nextLink;
+public abstract class AbstractLink {
+	protected AbstractLink nextLink;
+	
+	public AbstractLink getNextLink() {
+		return nextLink;
+	}
+
+	public void setNextLink(AbstractLink nextLink) {
+		this.nextLink = nextLink;
+	}
+	
+	
 }
